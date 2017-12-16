b15ddc96684b2481abe826fe0dcd26a5aafafa02
Brandon Johnson <bbj1979@gmail.com>
2017-12-16 01:01:34 -0700

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import App from './components/App.jsx'
+import Game from './components/Game.jsx'
+
+// ReactDOM.render(
+//   <App name="Yabba Dabba Doo" />,
+//   document.getElementById('app')
+// )
+
+ReactDOM.render(<Game />, document.getElementById("app"));
